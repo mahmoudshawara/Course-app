59d152236d23a71a043ff1573b48774cd6e6e399
Shawara <84108540+mahmoudshawara@users.noreply.github.com>
2022-01-22 22:43:36 +0200

    Setting DB url manually

--- config.py
+++ config.py
@@ -8,14 +8,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-user = os.environ['POSTGRES_USER']
-password = os.environ['POSTGRES_PASSWORD']
-host = os.environ['POSTGRES_HOST']
-database = os.environ['POSTGRES_DB']
-port = os.environ['POSTGRES_PORT']
+#user = os.environ['POSTGRES_USER']
+#password = os.environ['POSTGRES_PASSWORD']
+#host = os.environ['POSTGRES_HOST']
+#database = os.environ['POSTGRES_DB']
+#port = os.environ['POSTGRES_PORT']
 
-SQLALCHEMY_DATABASE_URI = f'postgresql://{user}:{password}@{host}:{port}/{database}'
+#SQLALCHEMY_DATABASE_URI = f'postgresql://{user}:{password}@{host}:{port}/{database}'
 
-#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/school'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:1234@localhost:5432/school'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
